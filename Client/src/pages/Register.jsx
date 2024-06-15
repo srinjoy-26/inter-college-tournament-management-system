5ecd9c5879f9f669a8c9e0999eabad9b91932cba
srinjoy-26 <srinjoydev26@gmail.com>
2024-06-15 12:25:59 +0530

    update register page

--- Client/src/pages/Register.jsx
+++ Client/src/pages/Register.jsx
@@ -2,8 +2,26 @@
 
 const Register = () => {
   return (
-    <div>
+    <div className='bg-black h-screen pt-[50px] '>
+    <form className='flex flex-col gap-4  p-4  w-[40%] mx-auto rounded-md'>
+      <h1 className='text-white text-2xl text-center'>Register</h1>
       
+      <input type="text" id="name" className='border-2 border-slate-600  p-2 focus:outline-none rounded-md bg-slate-800 placeholder:font-thin text-white' placeholder='name'/>
+
+      <input type="email" id="p" className='border-2 border-slate-600 p-2 focus:outline-none rounded-md bg-slate-800 placeholder:font-thin text-white' placeholder='email'/>
+
+      <input type="text" id="p" className='border-2 border-slate-600 p-2 focus:outline-none rounded-md bg-slate-800 placeholder:font-thin text-white' placeholder='username'/>
+
+      <input type="password" id="p" className='border-2 border-slate-600 p-2 focus:outline-none rounded-md bg-slate-800 placeholder:font-thin text-white' placeholder='password'/>
+      
+      <p className="text-white text-sm font-thin">Already have an account? <a href="./Login.html" className="text-blue-400">
+        Login</a></p>
+      
+     
+     
+
+      <button className='text-white py-3 rounded-md bg-blue-500 w-[30%] mx-auto'>Submit</button>
+    </form>
     </div>
   )
 }
